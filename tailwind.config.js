6f44fe40d5e31eef788f6724caf13fece25d7291
Burrito-Princess <willem.lol@outlook.com>
2023-12-12 20:36:20 +0100

    content div made

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}", "./pages/*.html"],
+  content: ["./dist/*.{html,js}", "./dist/*/*.{html,js}"],
   theme: {
     colors: {
       king_pink: '#e8b7d5',
       king_white: '#f2f2f2',
+      black: '#000000'
     },
     extend: {
+      width:{
+        main: '40rem',
+      },
       fontFamily: {
         'lato': ['lato', 'sans-serif'],
       },
